0056ce7d75e5465d614946b1378806935a2d4fd4
abdurrafay-emumba <abdur.rafay@emumba.com>
2026-02-26 15:43:52 +0500

    Improved functionality

--- google_spread_sheet.py
+++ google_spread_sheet.py
@@ -8,20 +8,21 @@ def authenticate_sheets_only(creds_file: str):
     creds = ServiceAccountCredentials.from_json_keyfile_name(creds_file, scope)
     return gspread.authorize(creds)
 
-def append_or_create_tab_by_id(sheet_name: str, rows: List[List]) -> None:
+def append_or_create_tab_by_id(sheet_id: str, sheet_name: str, rows: List[List]) -> None:
 
     """
     Function Definition: This function will receive the sheet name and the value to add to it
     If the sheet does not exist, it will create it and add the value to it.
     Also the values that will arrive will have the headers with them, so if the sheet is created the headers will be added as well.
     Otherwise the headers will be ignored and only the values will be added.
+    :param sheet_id:
     :param sheet_name:
     :param rows:
     :return:
     """
 
     client = authenticate_sheets_only("credentials.json")
-    spreadsheet_id = "1WYA5BD6vje5MffjorLkf6Qw7h7sYOTIvB-E8turebX0"
+    spreadsheet_id = sheet_id # "1GoqBcqTtdsmfDtbS-JAmQqGrjNu3S1Vg1GwVyZHeHRI"
 
     ss = client.open_by_key(spreadsheet_id)
     try:
