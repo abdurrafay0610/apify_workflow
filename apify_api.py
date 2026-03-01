22868ab3629d1a39b90a7b72501a9535fce5cbec
abdurrafay-emumba <abdur.rafay@emumba.com>
2026-03-02 03:00:39 +0500

    minor changes

--- apify_api.py
+++ apify_api.py
@@ -103,7 +103,9 @@ if __name__ == "__main__":
     industry_link = "https://www.linkedin.com/sales/search/company?query=(filters%3AList((type%3AINDUSTRY%2Cvalues%3AList((id%3A19%2Ctext%3ARetail%2520Apparel%2520and%2520Fashion%2CselectionType%3AINCLUDED)))%2C(type%3ACOMPANY_HEADCOUNT%2Cvalues%3AList((id%3AE%2Ctext%3A201-500%2CselectionType%3AINCLUDED)))))&sessionId=dgbhZuu3QVqdHItpMZuBFA%3D%3D&viewAllFilters=true"
     personal_link = "https://www.linkedin.com/sales/search/people?query=(filters%3AList((type%3ACURRENT_COMPANY%2Cvalues%3AList((id%3A35921%2CselectionType%3AINCLUDED)))%2C(type%3APERSONA%2Cvalues%3AList((id%3A1962673041%2CselectionType%3AINCLUDED)))))&sessionId=jEQhyR76Ri%2BNSaBgRe%2FbAg%3D%3D"
 
-    url = industry_link
+    sales_profile_url = "https://www.linkedin.com/sales/lead/ACwAACS4S2wBkbrgkQ3jn3hZ6V-1O_OebhAJ2IE,NAME_SEARCH,Q0EI?_ntb=eOE8nqq9RJ2BcnLapCI8dQ%3D%3D"
+
+    url = sales_profile_url
 
     results, meta = run_sales_nav_scraper(url, total_records=25, deep_scrape=False)
 
